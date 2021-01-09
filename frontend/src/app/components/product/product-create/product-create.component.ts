b5f62d444e353c190e360d764747263f95ea82ad
Raphael <raphaelmalinski@gmail.com>
2021-01-08 21:08:39 -0300

    Creates createProduct functionality connecting frontend with backend

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './products.model';
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -9,6 +10,11 @@ import { Router } from '@angular/router';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name: 'Produto de teste',
+    price: 125.99
+  }
+
   constructor(private productService: ProductService,
               private router : Router) { }
 
@@ -16,9 +22,11 @@ export class ProductCreateComponent implements OnInit {
     
   }
 
-
   createProduct(): void {
-    this.productService.showMessage('Produto criado!');
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado!');
+      this.router.navigate(['/products']);
+    });
   }
 
   cancel(): void {
